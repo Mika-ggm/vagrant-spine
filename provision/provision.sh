fdfaa2c853f5beed3515c123231fa5742e74894a
createproblem <createproblem@hushmail.com>
2016-02-20 20:43:15 +0100

    added mysql

---
+++ provision/provision.sh
@@ -0,0 +1,207 @@
+#!/bin/bash
+#
+# provision.sh
+#
+# This file is specified in Vagrantfile and is loaded by Vagrant as the primary
+# provisioning script whenever the commands `vagrant up`, `vagrant provision`,
+# or `vagrant reload` are used. It provides all of the default packages and
+# configurations included with Varying Vagrant Vagrants.
+
+# By storing the date now, we can calculate the duration of provisioning at the
+# end of this script.
+start_seconds="$(date +%s)"
+
+# PACKAGE INSTALLATION
+#
+# Build a bash array to pass all of the packages we want to install to a single
+# apt-get command. This avoids doing all the leg work each time a package is
+# set to install. It also allows us to easily comment out or add single
+# packages. We set the array as empty to begin with so that we can append
+# individual packages to it as required.
+apt_package_install_list=()
+
+# Start with a bash array containing all packages we want to install in the
+# virtual machine. We'll then loop through each of these and check individual
+# status before adding them to the apt_package_install_list array.
+apt_package_check_list=(
+    # mysql is the default database
+    mysql-server
+)
+
+### FUNCTIONS
+
+network_detection() {
+  # Network Detection
+  #
+  # Make an HTTP request to google.com to determine if outside access is available
+  # to us. If 3 attempts with a timeout of 5 seconds are not successful, then we'll
+  # skip a few things further in provisioning rather than create a bunch of errors.
+  if [[ "$(wget --tries=3 --timeout=5 --spider http://google.com 2>&1 | grep 'connected')" ]]; then
+    echo "Network connection detected..."
+    ping_result="Connected"
+  else
+    echo "Network connection not detected. Unable to reach google.com..."
+    ping_result="Not Connected"
+  fi
+}
+
+network_check() {
+  network_detection
+  if [[ ! "$ping_result" == "Connected" ]]; then
+    echo -e "\nNo network connection available, skipping package installation"
+    exit 0
+  fi
+}
+
+noroot() {
+  sudo -EH -u "vagrant" "$@";
+}
+
+profile_setup() {
+  # Copy custom dotfiles and bin file for the vagrant user from local
+  cp "/srv/config/bash_profile" "/home/vagrant/.bash_profile"
+
+  if [[ ! -d "/home/vagrant/bin" ]]; then
+    mkdir "/home/vagrant/bin"
+  fi
+
+  rsync -rvzh --delete "/srv/config/homebin/" "/home/vagrant/bin/"
+  chmod +x /home/vagrant/bin/*
+
+  echo " * Copied /srv/config/bash_profile                      to /home/vagrant/.bash_profile"
+  echo " * rsync'd /srv/config/homebin                          to /home/vagrant/bin"
+
+  # If a bash_prompt file exists in the VVV config/ directory, copy to the VM.
+  if [[ -f "/srv/config/bash_prompt" ]]; then
+    cp "/srv/config/bash_prompt" "/home/vagrant/.bash_prompt"
+    echo " * Copied /srv/config/bash_prompt to /home/vagrant/.bash_prompt"
+  fi
+}
+
+package_check() {
+  # Loop through each of our packages that should be installed on the system. If
+  # not yet installed, it should be added to the array of packages to install.
+  local pkg
+  local package_version
+
+  for pkg in "${apt_package_check_list[@]}"; do
+    package_version=$(dpkg -s "${pkg}" 2>&1 | grep 'Version:' | cut -d " " -f 2)
+    if [[ -n "${package_version}" ]]; then
+      space_count="$(expr 20 - "${#pkg}")" #11
+      pack_space_count="$(expr 30 - "${#package_version}")"
+      real_space="$(expr ${space_count} + ${pack_space_count} + ${#package_version})"
+      printf " * $pkg %${real_space}.${#package_version}s ${package_version}\n"
+    else
+      echo " *" $pkg [not installed]
+      apt_package_install_list+=($pkg)
+    fi
+  done
+}
+
+package_install() {
+  package_check
+
+  # MySQL
+  #
+  # Use debconf-set-selections to specify the default password for the root MySQL
+  # account. This runs on every provision, even if MySQL has been installed. If
+  # MySQL is already installed, it will not affect anything.
+  echo mysql-server mysql-server/root_password password "root" | debconf-set-selections
+  echo mysql-server mysql-server/root_password_again password "root" | debconf-set-selections
+
+  # Postfix
+  #
+  # Use debconf-set-selections to specify the selections in the postfix setup. Set
+  # up as an 'Internet Site' with the host name 'vvv'. Note that if your current
+  # Internet connection does not allow communication over port 25, you will not be
+  # able to send mail, even with postfix installed.
+  # echo postfix postfix/main_mailer_type select Internet Site | debconf-set-selections
+  # echo postfix postfix/mailname string vvv | debconf-set-selections
+
+  # Disable ipv6 as some ISPs/mail servers have problems with it
+  # echo "inet_protocols = ipv4" >> "/etc/postfix/main.cf"
+
+  # Provide our custom apt sources before running `apt-get update`
+  # ln -sf /srv/config/apt-source-append.list /etc/apt/sources.list.d/vvv-sources.list
+  # echo "Linked custom apt sources"
+
+  # Update all of the package references before installing anything
+  echo "Running apt-get update..."
+  apt-get update -y
+
+  # Install required packages
+  echo "Installing apt-get packages..."
+  apt-get install -y ${apt_package_install_list[@]}
+  # Clean up apt caches
+  apt-get clean
+}
+
+mysql_setup() {
+  # If MySQL is installed, go through the various imports and service tasks.
+  local exists_mysql
+
+  exists_mysql="$(service mysql status)"
+  if [[ "mysql: unrecognized service" != "${exists_mysql}" ]]; then
+    echo -e "\nSetup MySQL configuration file links..."
+
+    # Copy mysql configuration from local
+    cp "/srv/config/mysql-config/my.cnf" "/etc/mysql/my.cnf"
+    cp "/srv/config/mysql-config/root-my.cnf" "/home/vagrant/.my.cnf"
+
+    echo " * Copied /srv/config/mysql-config/my.cnf               to /etc/mysql/my.cnf"
+    echo " * Copied /srv/config/mysql-config/root-my.cnf          to /home/vagrant/.my.cnf"
+
+    # MySQL gives us an error if we restart a non running service, which
+    # happens after a `vagrant halt`. Check to see if it's running before
+    # deciding whether to start or restart.
+    if [[ "mysql stop/waiting" == "${exists_mysql}" ]]; then
+      echo "service mysql start"
+      service mysql start
+      else
+      echo "service mysql restart"
+      service mysql restart
+    fi
+
+    # IMPORT SQL
+    #
+    # Create the databases (unique to system) that will be imported with
+    # the mysqldump files located in database/backups/
+    if [[ -f "/srv/db-mysql/init-custom.sql" ]]; then
+      mysql -u "root" -p"root" < "/srv/db-mysql/init-custom.sql"
+      echo -e "\nInitial custom MySQL scripting..."
+    else
+      echo -e "\nNo custom MySQL scripting found in db-mysql/init-custom.sql, skipping..."
+    fi
+
+    # Setup MySQL by importing an init file that creates necessary
+    # users and databases that our vagrant setup relies on.
+    mysql -u "root" -p"root" < "/srv/db-mysql/init.sql"
+    echo "Initial MySQL prep..."
+
+    # Process each mysqldump SQL file in database/backups to import
+    # an initial data set for MySQL.
+    # "/home/vagrant/bin/db_import"
+  else
+    echo -e "\nMySQL is not installed. No databases imported."
+  fi
+}
+
+# SCRIPT
+
+network_check
+# Profile_setup
+echo "Bash profile setup and directories."
+profile_setup
+
+network_check
+# Package and Tools Install
+echo " "
+echo "Main packages check and install."
+package_install
+mysql_setup
+
+# And it's done
+end_seconds="$(date +%s)"
+echo "-----------------------------"
+echo "Provisioning complete in "$((${end_seconds} - ${start_seconds}))" seconds"
+echo "For further setup instructions, visit https://github.com/createproblem/vagrant-spine"
